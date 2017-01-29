4f84f983e0a4c78a086acb829cdae4a108763b96
William Duclot <william.duclot@gmail.com>
2017-01-29 22:38:43 +0100

    Detail the properties of each class

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
+// The parse() function (parse.js) expect a callback to which it will
+// send the collection of artists.
 parse(main);
+
+/**
+ * Properties of an Artist object:
+ * id
+ * name
+ * yearOfBirth
+ * yearOfDeath
+ * placeOfBirth ("city, country" | "country")
+ * placeOfDeath ("city, country" | "country")
+ * gender ("Female" | "Male")
+ * artworks (array of Artwork objects)
+ */
+
+/**
+ * Properties of an Artwork object:
+ * id
+ * artist (theoretically equivalent to its artist name)
+ * artistId (equivalent to its artist id)
+ * title
+ * medium
+ * year
+ * dimension (Dimension object)
+ */
+
+/**
+ * Properties of a Dimension object:
+ * width
+ * height
+ * depth
+ * unit
+ */
 
 function main(artists) {
     artists.forEach(function (artist) {
