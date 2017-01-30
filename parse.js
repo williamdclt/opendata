d4625c9babc28957ea95ef096ee1c6072af33e48
William Duclot <william.duclot@gmail.com>
2017-01-30 15:21:17 +0100

    fix parse

--- parse.js
+++ parse.js
@@ -33,7 +33,7 @@ function parse(callback) {
     d3.json("collection/collection.json",function (error, data) {
         if (error) throw error;
 
-        return data;
+        callback(data);
     });
 }
 
