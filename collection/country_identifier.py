8f29b659f88a8e534c2dc446526e5a26e02df33e
Pierre <presentpierre@gmail.com>
2017-02-01 12:12:00 +0100

    des trucs de oufs

--- collection/country_identifier.py
+++ collection/country_identifier.py
@@ -4,10 +4,31 @@ import json
 import requests
 
 class CountryContinent:
-	def __init__(self, countryName, countryCode):
+	def __init__(self, countryName, continentName=None):
 		self.countryName = countryName
+		self.continentName = continentName
+
+class CodeCountryContinent:
+	def __init__(self, countryName, countryCode):
+		self.countryContinent = CountryContinent(countryName)
 		self.countryCode = countryCode
-		self.continentName = None
+
+def getCountryInBuffer(text)
+	with open('buffer_countries.json') as json_data:
+		d = json.load(json_data)
+		if text not in d or d[text]["countryName"] is None:
+			return None
+		return CountryContinent(d[text]["countryName"], d[text]["continentName"])
+
+def appendInBuffer(text, contiCountry)
+	data = {}
+	data[text] = []
+	data[text].append({
+		'countryName': contiCountry.countryName,
+		'continentName': contiCountry.countryContinent
+		})
+	with open('buffer_countries.json', 'w') as outfile:
+		json.dump(data, outfile)
 
 def getCountry(text):
 	r = requests.get('http://api.geonames.org/searchJSON?q=' + text + '&username=OpenBoniData')
@@ -15,16 +36,22 @@ def getCountry(text):
 
 	for elem in d["geonames"]:
 		if elem["fcode"]=="PCLI":
-			return CountryContinent(elem["countryName"], elem["countryCode"])
+			return CodeCountryContinent(CountryContinent(elem["countryName"]), elem["countryCode"])
 
 	return None
 
 def getContinentCountry(text):
-	res = getCountry(text)
-	if res is None:
+	res = getCountryInBuffer(text)
+	if res is not None:
+		return res
+
+	codeRes = getCountry(text)
+	if codeRes is None:
+		appendInBuffer(text, CountryContinent(None))
 		return None
 	with open('countries.json') as json_data:
 		d = json.load(json_data)
-		continentCode = d["countries"][res.countryCode]["continent"]
-		res.continentName = d["continents"][continentCode]
-		return res
+		continentCode = d["countries"][codeRes.countryCode]["continent"]
+		codeRes.countryContinent.continentName = d["continents"][continentCode]
+		appendInBuffer(text, codeRes.countryContinent)
+		return codeRes.countryContinent
